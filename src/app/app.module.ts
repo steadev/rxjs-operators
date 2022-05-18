93724a250e43c4f1d7659d0f329de2bd09b829ec
mjLee <audwn0326@gmail.com>
2022-05-18 19:04:59 +0900

    initial ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CombinationComponent } from './combination/combination.component';
+import { ConditionComponent } from './condition/condition.component';
+import { ConversionComponent } from './conversion/conversion.component';
+import { FilteringComponent } from './filtering/filtering.component';
+import { MathComponent } from './math/math.component';
+import { UtilityComponent } from './utility/utility.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilteringComponent,
+    ConversionComponent,
+    CombinationComponent,
+    MathComponent,
+    UtilityComponent,
+    ConditionComponent
   ],
   imports: [
     BrowserModule,
