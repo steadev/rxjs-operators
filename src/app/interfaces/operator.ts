0659d33aa5473d73b72c281a9d810ea643dcdfaf
mjLee <audwn0326@gmail.com>
2022-05-22 22:15:09 +0900

    listup all operators

---
+++ src/app/interfaces/operator.ts
@@ -0,0 +1,76 @@
+export const CombinationOperators = [
+  'combineLatest',
+  'concat',
+  'merge',
+  'race',
+  'startWith',
+  'withLatestFrom',
+  'zip'
+]
+export const ConditionalOperators = [
+  'defaultIfEmpty',
+  'every',
+  'sequenceEqual'
+];
+export const CreationOperators = [
+  'from',
+  'interval',
+  'of',
+  'timer'
+]
+
+export const FilterOperators = [
+  'filter',
+  'first',
+  'last',
+  'take',
+  'takeUntil',
+  'takeWhile',
+  'takeLast',
+  'skip',
+  'skipUntil',
+  'skipWhile',
+  'debounce',
+  'debounceTime',
+  'distinct',
+  'distinctUntilChanged',
+  'throttle',
+  'throttleTime',
+  'find',
+  'findIndex',
+  'elementAt',
+  'ignoreElements',
+  'sample'
+];
+
+export const MathmeticalOperators = [
+  'count',
+  'max',
+  'min',
+  'reduce'
+];
+
+export const TransformationOperators = [
+  'buffer',
+  'bufferCount',
+  'bufferTime',
+  'bufferToggle',
+  'bufferWhen',
+  'concatMap',
+  'concatMapTo',
+  'map',
+  'mapTo',
+  'mergeMap',
+  'mergeMapTo',
+  'pairwise',
+  'pluck',
+  'repeat',
+  'scan',
+  'switchMap',
+  'switchMapTo'
+];
+
+export const UtilityOperators = [
+  'delay',
+  'delayWhen'
+]
