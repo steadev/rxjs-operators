9bd6c4a9c8cac0e4544fef965ab490932baf06e5
mjLee <audwn0326@gmail.com>
2022-05-29 22:30:54 +0900

    feat: add marble component inputs

--- src/app/common/marble-diagram/marble-diagram.component.ts
+++ src/app/common/marble-diagram/marble-diagram.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-marble-diagram',
   templateUrl: './marble-diagram.component.html',
   styleUrls: ['./marble-diagram.component.scss']
 })
-export class MarbleDiagramComponent implements OnInit {
+export class MarbleDiagramComponent implements OnInit, OnChanges, OnDestroy{
+
+  @Input() marble: string | number | undefined;
+  marbleList: {
+    marble: string | number;
+    addedAt: string;
+  }[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.hasOwnProperty('marble') && changes.marble.currentValue !== undefined) {
+
+    }
+  }
+
+  ngOnDestroy(): void {
+    
+  }
 }
